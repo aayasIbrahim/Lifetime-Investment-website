85b67c807b2ee9d44f70b4630d837b8c4121c1fa
Ayas Ibrahim <aayasabraham@gmail.com>
2025-07-07 04:23:18 +0600

    nav almost complete

---
+++ src/Header/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav.jsx";
+
+function Hero() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/Header/Nav.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../assets/header/Logo.png";
+import Emoji from "../assets/header/Frame 4392.png";
+import { FaRegPaperPlane } from "react-icons/fa";
+
+function Nav() {
+  return (
+    <>
+        <nav className="container w-full bg-black flex place-content-between mx-auto">
+          <div className="">
+            <img className="w-[291px] h-[94px]" src={logo} alt="" />
+          </div>
+          <div className="flex items-center">
+            <div>
+              <img className="h-[54px] w-[54px] " src={Emoji} alt="" />
+            </div>
+            <div className="hidden md:flex">
+              <a href="/">
+                {" "}
+                <div className="flex items-center justify-center ml-[50px] bg-[#1E90FF] w-[342px] h-[78px] border rounded rounded-lg hover:bg-[#01BEFE]">
+                  <span>
+                    <FaRegPaperPlane className="text-white w-[30.57pxpx] h-[25.58px] me-[7px]" />
+                  </span>
+                  <span className="font-titillium font-semibold text-[28px] leading-6 text-white">
+                    Join Us On Telegram
+                  </span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </nav>
+    </>
+  );
+}
+
+export default Nav;
